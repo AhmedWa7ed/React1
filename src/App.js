a69123f42487e65eb45c1d4660b41c9354c11704
AhmedWa7ed <123801087+AhmedWa7ed@users.noreply.github.com>
2023-09-14 18:11:22 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+// import { Outlet, RouterProvider,createBrowserRouter } from 'react-router-dom';
+// import Navear from './Navear';
+// import Home from './Navbar/Home';
+// import About from './Navbar/About';
+// let rounter = createBrowserRouter([
+//   {path:"" , element : <Navear/> , children:[
+//     {path: "home" , element: <Home/>},
+//     {path: "about" , element: <About/>},
+//   ]}
+// ])
+
+
+export default function App() {
+
+  return (
+    <div className="App">
+
+ sads
+    </div>
+  );
+}
+
